8a0acd924359417aae8ff6635cd63f3da50ade9b
Vlad Melnikov <vladmelnikov022@gmail.com>
2024-06-06 16:06:05 +0300

    Added "learn more" modal

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const openModalButtons = document.querySelectorAll('.open-modal'); // Вибираємо всі елементи
-const closeModalWindow = document.querySelector('.close-join-modal');
-const modalWindow = document.querySelector('.modal-window');
+const closeModalWindow = document.querySelector('.close-join-modal'); // Кнопка закриття вікна
+const modalWindow = document.querySelector('.modal-window'); // Модальне вікно
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -11,4 +11,22 @@ openModalButtons.forEach(button => {
 
 closeModalWindow.addEventListener('click', () => {
     modalWindow.classList.remove('active');
+});
+
+
+
+// Learn window
+const openLearnButtons = document.querySelectorAll('.open-learn');
+const closeLearnWindow = document.querySelector('.close-learn-modal');
+const learnWindow = document.querySelector('.learn-window');
+
+openLearnButtons.forEach(learnBtn => {
+    learnBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        learnWindow.classList.add('active');
+    });
+});
+
+closeLearnWindow.addEventListener('click', () => {
+    learnWindow.classList.remove('active');
 });
